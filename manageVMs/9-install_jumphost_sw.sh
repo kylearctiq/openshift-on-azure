ca37904a245aa60981162e03b72b50c8b4009a8d
Kyle Bassett <kyle.bassett@arctiq.ca>
2017-02-12 19:04:29 -0500

    Create 9-install_jumphost_sw.sh

---
+++ manageVMs/9-install_jumphost_sw.sh
@@ -0,0 +1,9 @@
+sudo bash
+yum install git -y
+yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
+yum install nodejs
+node -v -y
+npm -v -y
+npm install -g azure-cli
+git clone https://github.com/kylearctiq/openshift-on-azure.git
