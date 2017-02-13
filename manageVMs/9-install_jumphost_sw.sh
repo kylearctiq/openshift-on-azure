3dd1032906c22d4669b70e849a3bd8330d08351c
Kyle Bassett <kyle.bassett@arctiq.ca>
2017-02-12 22:33:50 -0500

    Update 9-install_jumphost_sw.sh

--- manageVMs/9-install_jumphost_sw.sh
+++ manageVMs/9-install_jumphost_sw.sh
@@ -9,3 +9,10 @@ npm install -g azure-cli
 git clone https://github.com/kylearctiq/openshift-on-azure.git
 cd openshift-on-azure/manageVMs/
 chmod +x -R *.sh
+
+
+#subscription-manager register
+#subscription-manager attach --pool <<your poolid>> 
+#subscription-manager repos --disable="*"
+#subscription-manager repos     --enable="rhel-7-server-rpms"     --enable="rhel-7-server-extras-rpms"     --enable="rhel-7-server-ose-3.4-rpms"
+#yum install -y atomic-openshift-utils
