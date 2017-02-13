9fdb1c4a5e0eb4176698c65c24c4ba9bd7cb4c8b
Kyle Bassett <kyle.bassett@arctiq.ca>
2017-02-12 22:28:23 -0500

    Create clean-docker2.sh

---
+++ v3.4/clean-docker2.sh
@@ -0,0 +1,3 @@
+rm -rf /etc/origin /var/lib/openshift /etc/etcd \
+    /var/lib/etcd /etc/sysconfig/atomic-openshift* /etc/sysconfig/docker* \
+    /root/.kube/config /etc/ansible/facts.d /usr/share/openshift
