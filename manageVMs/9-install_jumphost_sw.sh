2ecef7ad9632b1d935f15d508bde8aa851461870
Kyle Bassett <kyle.bassett@arctiq.ca>
2017-02-13 19:37:08 -0500

    Update 9-install_jumphost_sw.sh

--- manageVMs/9-install_jumphost_sw.sh
+++ manageVMs/9-install_jumphost_sw.sh
@@ -1,12 +1,10 @@
-bash
-yum install git -y
+sudo bash
 yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
 yum install nodejs
 node -v -y
 npm -v -y
 npm install -g azure-cli
-git clone https://github.com/kylearctiq/openshift-on-azure.git
 cd openshift-on-azure/manageVMs/
 chmod +x -R *.sh
 
